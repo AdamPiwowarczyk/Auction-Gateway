9431b19b92babfef005c4f754bb3766cb8dc4b0c
AdamPiwowarczyk <piwowarczykadam23@gmail.com>
2021-06-06 13:15:44 +0200

    first commit

---
+++ src/main/java/com/auction/gateway/GatewayApplication.java
@@ -0,0 +1,33 @@
+package com.auction.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.function.Function;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer configure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200", "http://localhost:4401")
+						.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
+	}
+
+}
