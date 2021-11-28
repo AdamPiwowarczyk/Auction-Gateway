e468e1f3365c09db1df6b006b569e86567499a90
AdamPiwowarczyk <piwowarczykadam23@gmail.com>
2021-11-28 18:17:45 +0100

    first commit

--- src/main/java/com/auction/gateway/GatewayApplication.java
+++ src/main/java/com/auction/gateway/GatewayApplication.java
@@ -8,26 +8,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.function.Function;
-
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 
-	@Bean
-	public WebMvcConfigurer configure() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200", "http://localhost:4401")
-						.allowedMethods("GET", "POST", "PUT", "DELETE");
-			}
-		};
-	}
+    @Bean
+    public WebMvcConfigurer configure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200", "http://localhost:4401")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+            }
+        };
+    }
 
 }
